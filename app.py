e20fda4b2ba0aefed2fc82dd060f650cd4e55779
Danilo <danilo.s_95@outlook.com>
2025-05-21 10:59:52 -0300

    Implementing user roles

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin123@127.0.0.1:3306/flask_crud"
+
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -28,7 +30,7 @@ def login():
        #login
        user = User.query.filter_by(username=username).first()
        
-       if user and user.password == password:
+       if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
         # Autenticar o usuário
            login_user(user)
            print(current_user.is_authenticated)
@@ -43,7 +45,7 @@ def logout():
     return jsonify({"message": "Logout realizado com sucesso!"})
     
 @app.route("/user", methods=["POST"])
-@login_required
+
 def create_user():
     data = request.json
     username = data.get("username")
@@ -51,7 +53,8 @@ def create_user():
 
     if username and password:
         # Cria um novo usuário
-        user = User(username=username, password=password)
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user")
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário criado com sucesso!"})
@@ -72,6 +75,9 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role == "user":
+        return jsonify({"message": "Você não tem permissão para atualizar este usuário"}), 403
+    
     if user and data.get("password"):
         # Atualiza o usuário
         user.password = data.get("password")
@@ -85,6 +91,9 @@ def update_user(id_user):
 def delete_user(id_user):
     user = User.query.get(id_user)
 
+    if current_user.role != "admin":
+        return jsonify({"message": "Operação não permitida"}), 403
+    
     # Verifica se o usuário é o mesmo que está logado
     if id_user == current_user.id:
         return jsonify({"message": "Você não tem permissão para deletar este usuário"}), 403
--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 from database import db
 from flask_login import UserMixin
 class User(db.Model, UserMixin):
-    # id(int), username(str), password(str)
+    # id(int), username(str), password(str), role(str)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True,nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default="user")
 
 
 
